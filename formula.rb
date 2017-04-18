b752b8648ab14dcefeece3e7a356f68e6be6d2b3
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-18 16:55:27 -0400

    update brew formula

--- formula.rb
+++ formula.rb
@@ -1,9 +1,9 @@
 class BSPInstall < Formula
   desc "Brightspot Server Installer"
   homepage "https://github.com/OttoHagg/brightspot-server-install"
-  url #TODO needs URL of release tar.gz
-  version #TODO needs release version number
-  sha256 #TODO checksum of tarball
+  url "https://github.com/p-easterbrooks/brightspot-initializer/archive/v0.1.tar.gz"
+  version "0.1"
+  sha256 "5465691d565088494ea44ac67ceb58d953ee25464bf0ff7f60bf6a465a65a0ee"
 
   def install
     bin.install "bsp-install"
