88c8cc12f35e37d337fab1ecde8f8e70cd07882f
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-18 17:02:18 -0400

    rename brew

--- bsp-install.rb
+++ bsp-install.rb
@@ -1,6 +1,6 @@
 class BSPInstall < Formula
   desc "Brightspot Server Installer"
-  homepage "https://github.com/OttoHagg/brightspot-server-install"
+  homepage "https://github.com/p-easterbrooks/brightspot-initializer"
   url "https://github.com/p-easterbrooks/brightspot-initializer/archive/v0.1.tar.gz"
   version "0.1"
   sha256 "5465691d565088494ea44ac67ceb58d953ee25464bf0ff7f60bf6a465a65a0ee"
