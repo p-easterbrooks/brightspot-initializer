6dde73e3a40fc8e5e8dea8bf8c0fc321b56e2413
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-18 17:11:12 -0400

    update repo references

--- formula.rb
+++ formula.rb
@@ -1,7 +1,7 @@
 class BSPInstall < Formula
   desc "Brightspot Server Installer"
-  homepage "https://github.com/p-easterbrooks/brightspot-initializer"
-  url "https://github.com/p-easterbrooks/brightspot-initializer/archive/v0.1.tar.gz"
+  homepage "https://github.com/p-easterbrooks/homebrew-brightspot-initializer"
+  url "https://github.com/p-easterbrooks/homebrew-brightspot-initializer/archive/v0.1.tar.gz"
   version "0.1"
   sha256 "5465691d565088494ea44ac67ceb58d953ee25464bf0ff7f60bf6a465a65a0ee"
 
