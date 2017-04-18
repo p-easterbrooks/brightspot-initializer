1df54a476ff8d7088fa871704c99f98a10dc9753
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-18 17:29:55 -0400

    correct checksum

--- installer.rb
+++ installer.rb
@@ -3,7 +3,7 @@ class Installer < Formula
   homepage "https://github.com/p-easterbrooks/homebrew-brightspot-initializer"
   url "https://github.com/p-easterbrooks/homebrew-brightspot-initializer/archive/v0.1.tar.gz"
   version "0.1"
-  sha256 "5465691d565088494ea44ac67ceb58d953ee25464bf0ff7f60bf6a465a65a0ee"
+  sha256 "980dc381d0b82851266c2972eedb6ddcf2611f82cc70ee1ceb3daec200ce1353"
 
   def install
     bin.install "bsp-install"
