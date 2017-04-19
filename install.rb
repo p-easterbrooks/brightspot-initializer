159067a8b1ce0db385d96eccb13dd43b6beced1d
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-19 15:28:46 -0400

    fix url

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 class Install < Formula
   desc "Brightspot Server Installer"
   homepage "https://github.com/p-easterbrooks/blit"
-  url "https://github.com/p-easterbrooks/blit/archive/v0.1.tar.gz"
+  url "https://github.com/p-easterbrooks/blit/archive/v0.2.2.tar.gz"
   version "0.2.2"
   sha256 "8244b718846244d0f0e2dbc80840bb6135d4df31ef8c4c999875895681a311ea"
 
