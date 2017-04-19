7de468764491da70a461445b0a31a734f34f64b9
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-19 15:23:00 -0400

    make changes to homebrew versioning

--- blit.rb
+++ blit.rb
@@ -1,11 +1,11 @@
-class Installer < Formula
+class BLIT < Formula
   desc "Brightspot Server Installer"
-  homepage "https://github.com/p-easterbrooks/homebrew-brightspot-initializer"
-  url "https://github.com/p-easterbrooks/homebrew-brightspot-initializer/archive/v0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "d37f83c5e7a8e4d9cb88027fc95a14d8ba3fbdb7fd8425523758501858bfec12"
+  homepage "https://github.com/p-easterbrooks/blit"
+  url "https://github.com/p-easterbrooks/blit/archive/v0.1.tar.gz"
+  version "0.1"
+  sha256 "12b32c0e2239c22aee6b612def99231d40e3157b6d6ed6bbc0855cfae817857e"
 
   def install
-    bin.install "bsit"
+    bin.install "blit"
   end
 end
