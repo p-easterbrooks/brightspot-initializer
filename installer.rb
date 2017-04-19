c931957afc67ec141cf10794bcb839e7e8b2100b
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-19 13:50:43 -0400

    homebrew update

--- installer.rb
+++ installer.rb
@@ -1,9 +1,9 @@
 class Installer < Formula
   desc "Brightspot Server Installer"
   homepage "https://github.com/p-easterbrooks/homebrew-brightspot-initializer"
-  url "https://github.com/p-easterbrooks/homebrew-brightspot-initializer/archive/v0.2.tar.gz"
-  version "0.2"
-  sha256 "0ee1ebe6b028e733e45318b07384a5aad3e97b74ebe83bb66fec6b50884de32c"
+  url "https://github.com/p-easterbrooks/homebrew-brightspot-initializer/archive/v0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "d37f83c5e7a8e4d9cb88027fc95a14d8ba3fbdb7fd8425523758501858bfec12"
 
   def install
     bin.install "bsit"
