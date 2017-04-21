b5ee4184e8a0d095350e6eba7d3f217968211bb6
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-04-21 17:01:22 -0400

    v0.3 release

--- blit.rb
+++ blit.rb
@@ -1,9 +1,9 @@
 class Blit < Formula
   desc "Brightspot Server Installer"
   homepage "https://github.com/p-easterbrooks/blit"
-  url "https://github.com/p-easterbrooks/blit/archive/v0.2.2.tar.gz"
-  version "0.2.2"
-  sha256 "8244b718846244d0f0e2dbc80840bb6135d4df31ef8c4c999875895681a311ea"
+  url "https://github.com/p-easterbrooks/blit/archive/v0.3.tar.gz"
+  version "0.3"
+  sha256 "88e6c2fd545a4ede9fbe6eed473cf9843e63ce056be6bc305d452e1398c55836"
 
   def install
     bin.install "blit"
