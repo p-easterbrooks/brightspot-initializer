20525bcce4bd096b8a60a26cb7a0b8350a951b4d
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2017-06-12 15:15:04 -0400

    release v0.5.2

--- blitz.rb
+++ blitz.rb
@@ -1,9 +1,9 @@
 class Blitz < Formula
   desc "Brightspot Server Installation Tool"
   homepage "https://github.com/p-easterbrooks/blitz"
-  url "https://github.com/p-easterbrooks/blitz/archive/v0.5.1.tar.gz"
-  version "0.5.1"
-  sha256 "67381dbc45b48df46e90c32f4a8700cae8ee86d67b215ee3818e15b5001978ea"
+  url "https://github.com/p-easterbrooks/blitz/archive/v0.5.2.tar.gz"
+  version "0.5.2"
+  sha256 "e3f005a8bbb2813f0112e18bd3f88a0be226b74ba7059e10917cfdaf17c58cc0"
 
   def install
     bin.install "blitz"
